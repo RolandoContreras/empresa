167e58a4a25723f57f660cf4afdb7290687f010d
unknown <software.contreras@gmail.com>
2014-10-15 23:36:19 -0700

    changes in backoffice controller

--- application/controllers/backoffice.php
+++ application/controllers/backoffice.php
@@ -5,13 +5,41 @@ class Backoffice extends CI_Controller {
 	
     function __construct() {
         parent::__construct();
+        $this->load->model("commissions_model","obj_commissions");
+        $this->load->model("customer_model","obj_customer");
     }
     
     public function index()
     {
         if(isset($_SESSION['customer'])){
              /// VISTA
-            $this->tmp_backoffice->render("backoffice/backoffice");
+            
+        $customer_id = $_SESSION['customer']['customer_id'];
+        $params = array(
+                        "select" =>"SUM(amount) as total",
+                         "where" => "parent_id = $customer_id and status_value = 0",
+                        );
+        
+        $total = $this->obj_commissions->get_search_row($params);
+        $this->tmp_backoffice->set("total",$total);
+
+        //SELECT CHILD CUSTOMER
+        $customer_id = $_SESSION['customer']['customer_id'];
+        $params = array(
+                        "select" =>"customer_id,
+                                    email,
+                                    password,
+                                    first_name,
+                                    status_value",
+                         "where" => "parents_id = $customer_id",
+                        ); 
+        
+                      
+        // VISTA
+        $obj_profile = $this->obj_customer->search($params);
+        
+        $this->tmp_backoffice->set("obj_profile",$obj_profile);
+        $this->tmp_backoffice->render("backoffice/backoffice");
         } else{
             redirect('micuenta');
         }  
--- application/views/backoffice/backoffice.php
+++ application/views/backoffice/backoffice.php
@@ -10,136 +10,52 @@
         <div class="row">
                 <div class="col-xs-12 col-md-7">
                 <section class="widget widget-points">
-
                     <div class="row">
-
-                        <div class="col-xs-4 .col-sm-4">
-
-                            <div class="wg-content">
-                                <h3>
-                                    <i class="icon-circle-arrow-left"></i>
-                                    Left Leg                                </h3>
-                                <p> 
-                                    1.000                                </p>
-                            </div>
-                        </div>
-
                         <div class="col-xs-4 .col-sm-4">
                             <div class="wg-content">
-                                <h3>
-                                    <i class="icon-circle-arrow-right"></i>
-                                    Right Leg                                </h3>
-                                <p> 
-                                    0                                </p>
+                                <h3>Balance</h3>
+                                <p><?php echo format_number($total->total);?></p>
                             </div>
                         </div>
-
-                        <div class="col-xs-4 .col-sm-4">
-                            <div class="wg-content">
-                                <h3>
-                                    <i class="icon-money"></i>
-                                    Balance                                </h3>
-                                <p>$266.00</p>
-                            </div>
-                        </div>
-
                     </div>
                 </section>
 
-
-
-                 Start My Unilevel 
+                  
                 <section class="widget" style="margin-top:15px;">
                     <header class="title-widget" style="margin-bottom:10px;">
-                        <h1>Unilevel Team</h1>
+                        <h1>Equipo Unilevel</h1>
                     </header>
                     <div class="wg-content">
                         <table class="table table-hover table-striped table-bordered tabela my-unilevel" style="text-align: center;">
                             <thead>
                                 <tr>
                                     <th><center>Name</center></th>
-                                    <th><center>Line</center></th>
                                     <th><center>Sponsor</center></th>
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php 
+                                if(count($obj_profile)>0){
+                                    foreach ($obj_profile as $value) { ?>
+                                     <tr>
+                                        <td><?php echo $value->first_name;?> ( <?php echo $value->email;?> )</td>
+                                        <td><?php echo $_SESSION['customer']['name'];?> ( <?php echo $_SESSION['customer']['email'];?> )</td>
+                                     </tr> 
+                                     
+                                <?php  }
                                 
-                                <tr>
-                                    <td>Pedro Perez ( pedroperezpalma )</td>
-                                    <td>1st Level</td>
-                                    <td>Rolando Contreras ( rolandocontreras )</td>
-                                </tr>
+                                }else{ ?>
+                                    <tr>
+                                        <td colspan="2">Usted No tiene referidos</td>
+                                     </tr>   
+                               <?php } ?>
                                 
-                                <tr>
-                                    <td>Roxana Lopez ( roxanacabana )</td>
-                                    <td>1st Level</td>
-                                    <td>Rolando Contreras ( rolandocontreras )</td>
-                                </tr>
                                 
-                                <tr>
-                                    <td>Rosario Perez ( pedroperezpalma2 )</td>
-                                    <td>2nd Level</td>
-                                    <td>Pedro Perez ( pedroperezpalma )</td>
-                                </tr>
                                 
-                                <tr>
-                                    <td>Mayra Romero ( pedroperezpalma3 )</td>
-                                    <td>2nd Level</td>
-                                    <td>Pedro Perez ( pedroperezpalma )</td>
-                                </tr>
-                                
-                                <tr>
-                                    <td>Veronica Javier ( roxanacabana3 )</td>
-                                    <td>2nd Level</td>
-                                    <td>Roxana Lopez ( roxanacabana )</td>
-                                </tr>
-                                
-                                <tr>
-                                    <td>Sandra Cabana ( roxanacabana2 )</td>
-                                    <td>2nd Level</td>
-                                    <td>Roxana Lopez ( roxanacabana )</td>
-                                </tr>
-                                
-                                <tr>
-                                    <td>Henrry Delgado ( henrryny1 )</td>
-                                    <td>3rd Level</td>
-                                    <td>Sandra Cabana ( roxanacabana2 )</td>
-                                </tr>
-                                
-                                <tr>
-                                    <td>Mariella Delgado ( henrryny2 )</td>
-                                    <td>4th Level</td>
-                                    <td>Henrry Delgado ( henrryny1 )</td>
-                                </tr>
-                                                            </tbody>
+                            </tbody>
                         </table>
                     </div>
                 </section>
-                 End My Unilevel 
-
-            </div>
-            <div class="col-xs-12 col-md-5">
-                 Start Points Report 
-                <section class="widget">
-                    <header class="title-widget" style="margin-bottom:10px;">
-                        <h1>Binary Points Report</h1>
-                    </header>
-                    <div class="wg-content">
-                        <table class="table table-hover table-striped table-bordered tabela" style="text-align: center;">
-                            <thead>
-                                <tr>
-                                    <th style="text-align: center;">Left</th>
-                                    <th style="text-align: center;">Right</th>
-                                    <th style="text-align: center;">Date</th>
-                                    <th style="text-align: center;">Comission</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <tr><td>1.500</td><td>1.000</td><td>2014-04-24</td><td>$500</td></tr><tr><td>1.500</td><td>500</td><td>2014-04-18</td><td>$250</td></tr>                            </tbody>
-                        </table>
-                    </div>
-                </section>
-                 End Points Report 
             </div>
         </div>
     </div>
