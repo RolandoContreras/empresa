3921574347fe1a6eea2a2ee976096931a51427e1
unknown <software.contreras@gmail.com>
2015-12-04 03:35:26 -0800

    changes in upgrade

--- application/controllers/b_upgrade.php
+++ application/controllers/b_upgrade.php
@@ -6,20 +6,25 @@ class B_upgrade extends CI_Controller {
     function __construct() {
         parent::__construct();
         $this->load->model("customer_model","obj_customer");
+        $this->load->model("franchise_model","obj_franchise");
     }
     
     public function index(){
         $this->get_session();
                 
         $customer_id = $_SESSION['customer']['customer_id'];
+        $franchise_id = $_SESSION['customer']['franchise_id'];
         //SELECT CUSTOMER_ID
         $params = array(
-                        "select" =>"franchise_id",
-                        "where" => "customer_id = $customer_id"
-                        ); 
-        $obj_profile = $this->obj_customer->get_search_row($params);
+                        "select" =>"franchise_id,name",
+                        "where" => "franchise.franchise_id > '$franchise_id'" ,
+                         ); 
+        $obj_upgrade = $this->obj_franchise->search($params);
         
-        $this->tmp_backoffice->set("obj_profile",$obj_profile);
+        var_dump($obj_upgrade);
+        die();
+        
+        $this->tmp_backoffice->set("obj_upgrade",$obj_upgrade);
         $this->tmp_backoffice->render("backoffice/upgrade");
     }
     
